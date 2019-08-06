14749347b01a308c96a26523eda4d66a9a6da7cf
Vivian <srtvivian@hotmail.com>
2019-08-06 11:12:34 -0300

    csharp essencial array

---
+++ WindowsFormsApp1/frmArray.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmArraay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblTamanhoArray = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(360, 152);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.BtnOk_Click);
+            // 
+            // lblTamanhoArray
+            // 
+            this.lblTamanhoArray.AutoSize = true;
+            this.lblTamanhoArray.Location = new System.Drawing.Point(360, 202);
+            this.lblTamanhoArray.Name = "lblTamanhoArray";
+            this.lblTamanhoArray.Size = new System.Drawing.Size(35, 13);
+            this.lblTamanhoArray.TabIndex = 1;
+            this.lblTamanhoArray.Text = "label1";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(360, 101);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoEllipsis = true;
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(273, 107);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 3;
+            this.lblNome.Text = "Nome:";
+            // 
+            // frmArraay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblTamanhoArray);
+            this.Controls.Add(this.btnOk);
+            this.Name = "frmArraay";
+            this.Text = "frmArraay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblTamanhoArray;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+    }
+}
---
+++ WindowsFormsApp1/frmArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmArraay : Form
+    {
+        public frmArraay()
+        {
+            InitializeComponent();
+        }
+
+        string[] alunos = new string[8];
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                if (alunos[i] == null)
+                {
+                    alunos[i] = txtNome.Text;
+                    txtNome.Text = string.Empty;
+                    break;
+                }
+            }
+        }
+    }
+}
